be9aef18912ab25bb47e1f697b2d25090bb8cdee
Veena Krishna Sandana <veenakrishna.s@gmail.com>
2020-10-08 00:10:07 -0700

    Reset link sent to email working

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,45 +1,95 @@
-const express = require('express');  
-const bcrypt = require('bcryptjs');
-const gravatar = require('gravatar');
-const User = require('../../models/User');
-const jwt= require('jsonwebtoken');
-const passport = require('passport');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
+const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
 
-const validateRegisterInput = require('../../validation/register');
-const validateLoginInput = require('../../validation/Login');
-const validateforgotpasswordInput = require('../../validation/forgotpassword');
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/Login");
+const validateforgotpasswordInput = require("../../validation/forgotpassword");
 const router = express.Router();
-const keys = require('../../config/keys');
+const keys = require("../../config/keys");
+const mailerKey = require('sendgrid')('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');
 
+console.log("using key" + mailerKey);
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: mailerKey,
+  })
+);
+
+if (!transporter) {
+  console.log("couldnt create transporter");
+}
+
+router.post("/resetpassword", (req, res) => {
+  // create token
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+    }
+    const mailtoken = buffer.toString("hex");
+    console.log(mailtoken);
+    const email = req.body.email;
+
+    User.findOne({ email })
+      .then((User) => {
+        if (!User) {
+          return res.status(404).json({ email: "User not found" });
+        }
+
+        //save token with a expiration time
+        User.resettoken = mailtoken;
+        User.expiretoken = Date.now() + 3600000;
+        User.save()
+          .then((res) => {
+            transporter.sendMail({
+              to: User.email,
+              from: "veenakrishna.s@gmail.com",
+              subject: " Password Reset",
+              html:
+                '<p>You or someone assesing your account has  requested for a password reset </p> <h5> Click this <a href="http://localhost:3000/forgotpassword/${token}"link >to reset your password</h5>',
+            });
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  });
+});
 
 //@route  POST/api/users/register
 //@desc   registers the user
 //@access public
-router.post('/register', (req, res) => {
-  const {errors, isValid} = validateRegisterInput(req.body);
+router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
 
-  if(!isValid){
+  if (!isValid) {
     return res.status(400).json(errors);
-  } 
+  }
 
   User.findOne({
     email: req.body.email,
   })
-  .then((user) => {
-    if (user) {
-    return res.status(400).json({email: 'Email Already Exists'});
-  } else {
-    const avatar = gravatar.url(req.body.email, {
-    s:'200',
-    r:'pg',
-    d:'mm'
-  });
-  const newUser = new User({
-    name: req.body.name,
-    email: req.body.email,
-    avatar: avatar,
-    password: req.body.password
-  });
+    .then((user) => {
+      if (user) {
+        return res.status(400).json({ email: "Email Already Exists" });
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: "200",
+          r: "pg",
+          d: "mm",
+        });
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar: avatar,
+          password: req.body.password,
+        });
 
         bcrypt.genSalt(10, (err, salt) => {
           if (err) throw err;
@@ -55,14 +105,13 @@ router.post('/register', (req, res) => {
       }
     })
     .catch((err) => console.log(err));
-})
+});
 
 //@route  POST/api/users/login
 //@descr  Logs user in
 //@access Public
 
-router.post('/login', (req, res) => {
-
+router.post("/login", (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
 
   if (!isValid) {
@@ -115,21 +164,19 @@ router.post('/login', (req, res) => {
 //@desc Return current user info
 //@access Private
 
-router.get('/current',
-  passport.authenticate('jwt', {session:false}),
-  (req,res)=>{
-
-   return res.json(req.user);
-
-});
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 //@route POST /api/users/forgotpassword
 //@desc resetting password
 //@access Public
 
-router.post('/forgotpassword',
-async (req,res)=>{
-
+router.post("/forgotpassword", async (req, res) => {
   const { errors, isValid } = validateforgotpasswordInput(req.body);
 
   if (!isValid) {
@@ -137,50 +184,42 @@ async (req,res)=>{
   }
   const email = req.body.email;
   let newpassword = req.body.password;
-  
-  console.log("before hash:",newpassword);
 
-  
+  console.log("before hash:", newpassword);
+
   //Find user with email
   User.findOne({ email })
-  .then( (user)=>{
-  if(!user){
-  return res.status(404).json({ email: "User not found"});  
-  }
-  let userid = user._id;
-    bcrypt.genSalt(10, (err, salt) =>{
-      if (err) throw err;
-      bcrypt.hash(newpassword, salt, (err, hash)=>{
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ email: "User not found" });
+      }
+      let userid = user._id;
+      bcrypt.genSalt(10, (err, salt) => {
         if (err) throw err;
+        bcrypt.hash(newpassword, salt, (err, hash) => {
+          if (err) throw err;
           console.log(" hash:", hash);
-       
+
           newpassword = hash;
           console.log("after hash:", newpassword);
-       
-       
+
           //update user new password in DataBase
-       
-         User.findOneAndUpdate(
-           { 
-             _id: userid
-             },
+
+          User.findOneAndUpdate(
             {
-               password: newpassword
-             })
+              _id: userid,
+            },
+            {
+              password: newpassword,
+            }
+          )
 
-           .then((user) => res.json({Msg:'User Password Updated'}))
-           .catch((err) => console.log(err));
+            .then((user) => res.json({ Msg: "User Password Updated" }))
+            .catch((err) => console.log(err));
+        });
       });
-      
     })
- 
-  })
-  .catch((err)=>console.log(err));
-
-
-
+    .catch((err) => console.log(err));
 });
-  
-  
 
 module.exports = router;
